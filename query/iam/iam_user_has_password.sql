abca49781fdfac09bfbf1cbde90a91a02a0c5d42
LalitTurbot <lalit@turbot.com>
2022-03-29 14:37:36 +0530

    update iam_user_has_password.sql

--- query/iam/iam_user_has_password.sql
+++ query/iam/iam_user_has_password.sql
@@ -8,24 +8,20 @@ with users_with_account_admin_role as (
     and granted_to = 'USER'
 )
 select
+  -- Required Columns
   name as resource,
-  -- TODO: Skip users without ACCOUNTADMIN role with specific reason
-  case when has_password then
-    'alarm'
-  else
-    'ok'
+  case
+    when name not in (select * from users_with_account_admin_role) then 'skip'
+    when has_password then 'alarm'
+    else 'ok'
   end as status,
-  case when has_password then
-    name || ' has password set.'
-  else
-    name || ' does not have password set.'
+  case
+    when name not in (select * from users_with_account_admin_role) then name || ' does not have ACCOUNTADMIN role.'
+    when has_password then name || ' has password set.'
+    else name || ' does not have password set.'
   end as reason,
+  -- Additional columns
   account
 from
-  snowflake_user
-where
-  name not in (
-    select
-      *
-    from
-      users_with_account_admin_role);
+  snowflake_user;
+
